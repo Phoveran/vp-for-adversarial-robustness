d64ac29a6a5c2619c4538f7f7954c99c842ae4a2
Arthur Chen <chenaoch@msu.edu>
2022-10-26 20:05:13 -0400

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -3,4 +3,4 @@ import os
 base_path = f"{os.getenv('HOME')}/workspace"
 
 data_path = os.path.join(base_path, "data")
-results_path = os.path.join(base_path, "results", "prompt-test-defense")
+results_path = os.path.join(base_path, "results", "vp-for-adversarial-robustness")
